9ad4cb172766f8633a24cb6925f5b5281b18be56
zhyeong <z.hyeong@kaist.ac.kr>
2020-12-12 14:51:53 +0900

    front end updated

--- frontend/Foot.js
+++ frontend/Foot.js
@@ -2,60 +2,92 @@ var left = document.getElementById("leftFoot");
 var right = document.getElementById("rightFoot");
 var moving = false;
 
+left.style.left=20%
+
 left.addEventListener("mousedown", initialClick, false);
 right.addEventListener("mousedown", initialClick, false);
-
+current_rotation_left_foot=0
+current_rotation_right_foot=0
 
 function move(e){
+if (e.type=='mousemove'){    
 
-  var newX = e.clientX - 10;
-  var newY = e.clientY - 10;
-
+var newX = e.clientX -50;
+  var newY = e.clientY - 100;
   image.style.left = newX + "px";
-  image.style.top = newY + "px";
+  image.style.top = newY + "px";}
 
-  
+  else { //rotate
+    console.log(e.keyCode, image.id)
+    if(e.keyCode==37 && image.id=='rightFoot'){
+        current_rotation_right_foot-=10
+        image.style.transform='rotate('+current_rotation_right_foot+'deg)'}
+
+    else if(e.keyCode==39 && image.id=='rightFoot'){
+            current_rotation_right_foot+=10
+            image.style.transform='rotate('+current_rotation_right_foot+'deg)'}
+    
+    else if(e.keyCode==37 && image.id=='leftFoot'){
+            current_rotation_left_foot-=10
+            image.style.transform='rotate('+current_rotation_left_foot+'deg)'}
+    
+    else if(e.keyCode==39 && image.id=='leftFoot'){
+                current_rotation_left_foot+=10
+                image.style.transform='rotate('+current_rotation_left_foot+'deg)'}
+    
+    
+    else if(e.keyCode==38 && image.id=='leftFoot'){
+        image.innerHTML='<img id="leftfoot_image" src="image/1_left_half.jpg"/>'}
+    else if(e.keyCode==40 && image.id=='leftFoot'){
+        image.innerHTML='<img id="leftfoot_image" src="image/1_left.jpg"/>'}
+    else if(e.keyCode==38 && image.id=='rightFoot'){
+        image.innerHTML='<img id="leftfoot_image" src="image/1_right_half.jpg"/>'}
+    else if(e.keyCode==40 && image.id=='rightFoot'){
+        image.innerHTML='<img id="leftfoot_image" src="image/1_right.jpg"/>'}
+
+      }
 }
 
-function initialClick(e) {
 
+function initialClick(e) {
+  window.scrollTo(0,0)
   if(moving){
     document.removeEventListener("mousemove", move);
-    moving = !moving;
+    window.removeEventListener("keydown",move)
+    console.log('end')
+    moving = !moving; 
     return;
   }
   
   moving = !moving;
   image = this;
-
+  window.addEventListener("keydown",move,false)  
   document.addEventListener("mousemove", move, false);
-console.log("inital")
+  
+  console.log("inital")
 }
 
-// code from https://stackoverflow.com/questions/33948464/move-an-image-with-javascript-using-mouse-events
+// image moving code from https://stackoverflow.com/questions/33948464/move-an-image-with-javascript-using-mouse-events
+
+
+function choose_sample(n){
+  left.innerHTML='<img id="leftfoot_image" src="image/'+n+'_left.jpg"/>'
+  right.innerHTML='<img id="rightfoot_image" src="image/'+n+'_right.jpg"/>'
+}
+
 
 function screenshot(){
     console.log('screenshot')    
-    let div = 
-            document.getElementById('square_outline'); 
+    let div = document.getElementById('square_outline'); 
 
         // Use the html2canvas 
         // function to take a screenshot 
         // and append it 
         // to the output div 
-   
-        // html2canvas(div,{allowTaint : true}).then( 
-        //     function (canvas) { 
-        //         canvas.crossOrigin=
-        //         var link=document.getElementById('output')
-        //         link.appendChild(canvas);
-        //         link.download = "html_image.png";
-        //         link.href = canvas.toDataURL("image/png");
-        //         // link.target = '_blank';
-        //         // link.click();
-
-        //     }) 
+        window.scrollTo(0,0)
             html2canvas(div,{allowTaint:true}).then(function(canvas) {
+                var output=document.getElementById('output')
+                output.appendChild(canvas)
                 var link = document.createElement("a");
                 document.body.appendChild(link);
                 link.download = "html_image.png";
